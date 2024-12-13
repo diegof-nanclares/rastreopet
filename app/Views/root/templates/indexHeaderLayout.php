17f7fe518c1dff5e27314eed4dcfab6e4d6bfc45
Diego Salazar <diego.salazarnanclar@dish.com>
2024-12-12 20:49:04 -0500

    Event zoom prevent default

--- app/Views/root/templates/indexHeaderLayout.php
+++ app/Views/root/templates/indexHeaderLayout.php
@@ -38,4 +38,23 @@
     </nav>
 </header>
 
+<script>
+    document.addEventListener('touchstart', function(event) {
+        if (event.touches.length > 1) {
+            // Si hay más de un dedo tocando la pantalla, no hacer zoom
+            event.preventDefault();
+        }
+    }, { passive: false });
+
+    // Detectar y prevenir el doble toque para zoom
+    var lastTouchEnd = 0;
+    document.addEventListener('touchend', function(event) {
+        var now = (new Date()).getTime();
+        if (now - lastTouchEnd <= 300) {
+            event.preventDefault(); // Prevenir el zoom
+        }
+        lastTouchEnd = now;
+    }, false);
+</script>
+
 
